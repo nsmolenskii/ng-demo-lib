d75130a6bcd859b3ade688577cb55b1302fa0c0e
Nikita Smolenskii <nikita.smolenskii@zalando.de>
2017-05-15 10:59:32 +0200

    (optional) use greeter in sandbox application

--- lib/sandbox/app/app.component.spec.ts
+++ lib/sandbox/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 
 import { AppComponent } from './app.component';
+import { GreeterModule } from '../../greeter';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +9,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        GreeterModule
+      ]
     }).compileComponents();
   }));
 
@@ -17,16 +21,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'demo works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('demo works!');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('demo works!');
+    expect(compiled.querySelector('demo-greeter').textContent).toContain('Greetings, John Doe!');
   }));
 });
--- lib/sandbox/app/app.module.ts
+++ lib/sandbox/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { GreeterModule } from '../../greeter';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    GreeterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
